5f5c0176f22b47f98f19d6f60b52237a50da0a1b
shujer <dwellerofuniverse@gmail.com>
2021-01-17 13:05:28 +0800

    feat: add buildTree

---
+++ src/buildTree.ts
@@ -0,0 +1,58 @@
+export const buildTree = <
+  ID extends string,
+  PID extends string,
+  T extends { [key in ID | PID]: string }
+>(
+  items: T[],
+  options: {
+    idKey: ID;
+    parentKey: PID;
+    rootVal?: string;
+    sort?(a: T, b: T): -1 | 1 | 0;
+  }
+) => {
+  const { idKey, parentKey, rootVal } = options;
+
+  type Wrapper = Map<string, T[]>;
+  type TreeNode = T & { children?: TreeNode[] };
+
+  const parnetWrap = (items: T[]): Wrapper => {
+    let parnetWrapper: Wrapper = new Map();
+    items.forEach((item) => {
+      if (parnetWrapper.has(item[parentKey])) {
+        let children = parnetWrapper.get(item[parentKey]) as T[];
+        children.push(item);
+      } else {
+        parnetWrapper.set(item[parentKey], [item]);
+      }
+    });
+    return parnetWrapper;
+  };
+
+  const topLevelWrap = (items: T[]): T[] => {
+    if (rootVal) {
+      return items.filter((ele) => ele[parentKey] === rootVal);
+    } else {
+      return items.filter(
+        //@ts-ignore
+        (parent) => !items.find((item) => item[idKey] === parent[parentKey])
+      );
+    }
+  };
+
+  const build = (topLevel: T[], wrapper: Wrapper): TreeNode[] => {
+    return topLevel.map((item) => {
+      if (wrapper.has(item[idKey])) {
+        return {
+          ...item,
+          children: build(wrapper.get(item[idKey]) as TreeNode[], wrapper),
+        };
+      } else {
+        return item;
+      }
+    });
+  };
+  const parnetWrapper = parnetWrap(items);
+  const topLevelWrapper = topLevelWrap(items);
+  return build(topLevelWrapper, parnetWrapper);
+};
